e694284f4fabd5c11d04799bdc2634bfb2465cc7
Vitalii Mostivskyi <vitaliymostivskiy@gmail.com>
2015-12-13 17:44:58 +0100

    Refactor updateAssets method.

--- app/src/main/java/ua/com/mostivskyi/vitalii/tessaracttestapp/Helpers/FileHelper.java
+++ app/src/main/java/ua/com/mostivskyi/vitalii/tessaracttestapp/Helpers/FileHelper.java
@@ -1,7 +1,6 @@
 package ua.com.mostivskyi.vitalii.tessaracttestapp.Helpers;
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.os.Environment;
 import android.util.Log;
 
@@ -14,60 +13,67 @@ import java.io.OutputStream;
 import ua.com.mostivskyi.vitalii.tessaracttestapp.R;
 
 public final class FileHelper {
+    private static final String UpdateAssetsTag = "Update assets";
+    private static final String CreateDirectoriesTag = "Create directories";
+
+    private static final String TrainDataExtension = ".traineddata";
 
     public static void UpdateAssets(Context context) {
-        String TAG = "Update assets";
         String assetsPath = Environment.getExternalStorageDirectory().toString() + context.getResources().getString(R.string.AssetsFilePath);
         String trainDataPath = context.getResources().getString(R.string.TrainDataPath);
-        String capturedImagePath = assetsPath + context.getResources().getString(R.string.CapturedImageName);
         String lang = context.getResources().getString(R.string.Language);
 
+        //create directories for train data (if not exists)
         String[] paths = new String[]{assetsPath, assetsPath + trainDataPath};
+        createDirectories(paths);
 
-        for (String path : paths)
+        String trainDataFileName = lang + TrainDataExtension;
+        String trainDataFilePath = assetsPath + trainDataPath + trainDataFileName;
+
+        //copy drain data file from assets to sd card ((if not exists))
+        if (!(new File(trainDataFilePath)).exists())
         {
+            try
+            {
+                InputStream in = context.getAssets().open(trainDataPath + trainDataFileName);
+                OutputStream out = new FileOutputStream(trainDataFilePath);
+                copyFile(in, out);
+
+                Log.v(UpdateAssetsTag, "Copied " + lang + " traineddata");
+            } catch (IOException e) {
+                Log.e(UpdateAssetsTag, "Was unable to copy " + lang + " traineddata - " + e.toString());
+            }
+        }
+    }
+
+    private static void createDirectories(String[] paths)
+    {
+        for (String path : paths) {
             File dir = new File(path);
 
-            if (!dir.exists())
-            {
-                if (!dir.mkdirs())
-                {
-                    Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(CreateDirectoriesTag, "ERROR: Creation of directory " + path + " on sdcard failed");
 
                     return;
-                }
-                else
-                {
-                    Log.v(TAG, "Created directory " + path + " on sdcard");
+                } else {
+                    Log.v(CreateDirectoriesTag, "Created directory " + path + " on sdcard");
                 }
             }
         }
+    }
 
-        // lang.traineddata file with the app (in assets folder)
-        // This area needs work and optimization
-        if (!(new File(assetsPath + "tessdata/" + lang + ".traineddata")).exists()) {
-            try {
-                AssetManager assetManager = context.getAssets();
-                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
-                //GZIPInputStream gin = new GZIPInputStream(in);
-                OutputStream out = new FileOutputStream(assetsPath
-                        + "tessdata/" + lang + ".traineddata");
+    private static void copyFile(InputStream inps, OutputStream outps) throws IOException
+    {
+        byte[] buffer = new byte[1024];
+        int length;
 
-                // Transfer bytes from in to out
-                byte[] buf = new byte[1024];
-                int len;
-                //while ((lenf = gin.read(buff)) > 0) {
-                while ((len = in.read(buf)) > 0) {
-                    out.write(buf, 0, len);
-                }
-                in.close();
-                //gin.close();
-                out.close();
-
-                Log.v(TAG, "Copied " + lang + " traineddata");
-            } catch (IOException e) {
-                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
-            }
+        while ((length = inps.read(buffer)) > 0)
+        {
+            outps.write(buffer, 0, length);
         }
+
+        inps.close();
+        outps.close();
     }
 }
