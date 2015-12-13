f9500334af702e96f0bcae0567920b1ff00ca214
Anet <anetyurchak@gmail.com>
2015-12-13 23:59:56 +0100

    cropping a photo

--- app/src/main/java/ua/com/mostivskyi/vitalii/tessaracttestapp/activities/StartActivity.java
+++ app/src/main/java/ua/com/mostivskyi/vitalii/tessaracttestapp/activities/StartActivity.java
@@ -1,10 +1,12 @@
 package ua.com.mostivskyi.vitalii.tessaracttestapp.activities;
 
 import java.io.File;
+import java.io.FileOutputStream;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -13,6 +15,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.isseiaoki.simplecropview.CropImageView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -24,110 +29,151 @@ import ua.com.mostivskyi.vitalii.tessaracttestapp.services.OCREngine;
 
 public class StartActivity extends Activity {
 
-    private static final String TAG = "TesseractTestApp";
-    private static final String PhotoTakenInstanceStateName = "photo_taken";
+  private static final String TAG = "TesseractTestApp";
+  private static final String PhotoTakenInstanceStateName = "photo_taken";
 
-    private OCREngine OCREngine;
+  private OCREngine OCREngine;
 
-    private String assetsPath;
-    private String capturedImagePath;
-    private String lang;
-    private boolean isPhotoTaken;
+  private String assetsPath;
+  private String capturedImagePath;
+  private String capturedCropImagePath;
+  private String lang;
+  private boolean isPhotoTaken;
+  Bitmap bitmap;
 
-    @Bind(R.id.recognizedTextField) EditText recognizedTextField;
-    @Bind(R.id.takePhotoButton) Button takePhotoButton;
+  @Bind(R.id.recognizedTextField)
+  EditText recognizedTextField;
+  @Bind(R.id.takePhotoButton)
+  Button takePhotoButton;
+  @Bind(R.id.crop_button)
+  Button cropButton;
+  @Bind(R.id.cropImageView)
+  CropImageView cropImageView;
+  @Bind(R.id.croppedImageView)
+  ImageView croppedImageView;
 
-    @OnClick(R.id.takePhotoButton)
-    public void takePhotoButtonClick(View view) {
-        Log.v(TAG, "Starting Camera app");
-        startCameraActivity();
+  @OnClick(R.id.takePhotoButton)
+  public void takePhotoButtonClick(View view) {
+    Log.v(TAG, "Starting Camera app");
+    startCameraActivity();
+  }
+
+  @OnClick(R.id.crop_button)
+  public void cropButtonClick(View view) {
+    Log.v(TAG, "Cropping");
+    croppedImageView.setImageBitmap(cropImageView.getCroppedBitmap());
+    createCroppedBitmap();
+    onPhotoTaken();
+  }
+
+  private void createCroppedBitmap(){
+
+    croppedImageView.setDrawingCacheEnabled(true);
+    bitmap = cropImageView.getCroppedBitmap();
+
+    File file = new File(capturedCropImagePath);
+    try
+    {
+      file.createNewFile();
+      FileOutputStream ostream = new FileOutputStream(file);
+      bitmap.compress(Bitmap.CompressFormat.JPEG, 100, ostream);
+      ostream.close();
     }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
-        ButterKnife.bind(this);
-
-        Initialize();
+    catch (Exception e)
+    {
+      e.printStackTrace();
     }
+  }
 
-    private void Initialize() {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_start);
+    ButterKnife.bind(this);
 
-        SetUpGlobals();
-        FileHelper.UpdateAssets(this);
-        InitializeOCR();
+    initialize();
+  }
+
+  private void initialize() {
+
+    setUpGlobals();
+    FileHelper.UpdateAssets(this);
+    initializeOCR();
+  }
+
+  private void initializeOCR() {
+
+    OCREngine = new OCREngine(assetsPath, lang);
+    OCREngine.setDebug(true);
+  }
+
+  private void setUpGlobals() {
+    assetsPath = Environment.getExternalStorageDirectory().toString() + getResources().getString(R.string.AssetsFilePath);
+    capturedImagePath = assetsPath + getResources().getString(R.string.CapturedImageName);
+    capturedCropImagePath = assetsPath + getResources().getString(R.string.CapturedCropImageName);
+
+    lang = getResources().getString(R.string.Language);
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    Log.i(TAG, "onActivity resultCode: " + resultCode);
+
+    if (resultCode == -1) {
+      performCrop();
+    } else {
+      Log.v(TAG, "User cancelled");
     }
+  }
 
-    private void InitializeOCR() {
+  protected void performCrop() {
+    cropImageView.setCropMode(CropImageView.CropMode.RATIO_FREE);
+    cropImageView.setImageBitmap(BitmapFactory.decodeFile(capturedImagePath));
+  }
 
-        OCREngine = new OCREngine(assetsPath, lang);
-        OCREngine.setDebug(true);
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    outState.putBoolean(StartActivity.PhotoTakenInstanceStateName, isPhotoTaken);
+  }
+
+  @Override
+  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    Log.i(TAG, "onRestoreInstanceState()");
+
+    if (savedInstanceState.getBoolean(StartActivity.PhotoTakenInstanceStateName)) {
+      onPhotoTaken();
     }
+  }
 
-    private void SetUpGlobals() {
-        assetsPath = Environment.getExternalStorageDirectory().toString() + getResources().getString(R.string.AssetsFilePath);
-        capturedImagePath = assetsPath + getResources().getString(R.string.CapturedImageName);;
-        lang = getResources().getString(R.string.Language);
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    OCREngine.close();
+  }
+
+  protected void startCameraActivity() {
+    File file = new File(capturedImagePath);
+    Uri outputFileUri = Uri.fromFile(file);
+
+    final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+    startActivityForResult(intent, 0);
+  }
+
+  protected void onPhotoTaken() {
+    isPhotoTaken = true;
+
+    Bitmap bitmap = ImageHelper.getCapturedImage(capturedImagePath);
+
+    Log.v(TAG, "Before OCREngine");
+    String recognizedText = OCREngine.recognize(bitmap);
+    Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+    if (recognizedText.length() > 0) {
+      recognizedTextField.setText(recognizedText);
+      recognizedTextField.setSelection(recognizedTextField.getText().toString().length());
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        Log.i(TAG, "onActivity resultCode: " + resultCode);
-
-        if (resultCode == -1)
-        {
-            onPhotoTaken();
-        }
-        else
-        {
-            Log.v(TAG, "User cancelled");
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putBoolean(StartActivity.PhotoTakenInstanceStateName, isPhotoTaken);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        Log.i(TAG, "onRestoreInstanceState()");
-
-        if (savedInstanceState.getBoolean(StartActivity.PhotoTakenInstanceStateName)) {
-            onPhotoTaken();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        OCREngine.close();
-    }
-
-    protected void startCameraActivity() {
-        File file = new File(capturedImagePath);
-        Uri outputFileUri = Uri.fromFile(file);
-
-        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
-
-        startActivityForResult(intent, 0);
-    }
-
-    protected void onPhotoTaken() {
-        isPhotoTaken = true;
-
-        Bitmap bitmap = ImageHelper.getCapturedImage(capturedImagePath);
-
-        Log.v(TAG, "Before OCREngine");
-        String recognizedText = OCREngine.recognize(bitmap);
-        Log.v(TAG, "OCRED TEXT: " + recognizedText);
-
-        if (recognizedText.length() > 0) {
-            recognizedTextField.setText(recognizedText);
-            recognizedTextField.setSelection(recognizedTextField.getText().toString().length());
-        }
-    }
+  }
 }
